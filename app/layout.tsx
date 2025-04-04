2cddfe90d5ebd296fbd6e10851b796616364c1d1
Mauricio Cucco <mscucco@hotmail.com>
2025-04-03 21:00:19 -0300

    Fix meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,5 +37,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-import "./globals.css";
--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -186,10 +186,10 @@ export function ContactForm() {
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
-              Contáctanos
+              Contactános
             </h2>
             <p className="max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-              ¿Tienes alguna pregunta? Completa el formulario y te responderemos
+              ¿Tenés alguna pregunta? Completá el formulario y te responderemos
               a la brevedad
             </p>
           </div>
